4e9531ee11e82b767523197f3997158baac7a753
YumeAyai <93071993+YumeAyai@users.noreply.github.com>
2025-11-15 02:56:46 +0800

    add feature - add new functionality to KETEX render in edit mode

--- main.ts
+++ main.ts
@@ -1,134 +1,85 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
+import katex from 'katex';
+import { StateField } from '@codemirror/state';
+import { EditorView, Decoration, DecorationSet, WidgetType } from '@codemirror/view';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class StandardLaTeXPlugin extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEditorExtension(latexHighlightExtension);
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+const LATEX_REGEX = /\\\((.*?)\\\)|\\\[(.*?)\\\]/gs;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class LatexWidget extends WidgetType {
+  match: RegExpExecArray;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  constructor(match: RegExpExecArray) {
+    super();
+    this.match = match;
+  }
+
+  toDOM(view: EditorView): HTMLElement {
+    const span = document.createElement('span');
+    const expr = this.match[1] ?? this.match[2] ?? '';
+    const displayMode = this.match[2] !== undefined;
+
+    // 可选：加个类名便于调试
+    span.className = displayMode ? 'cm-latex-block' : 'cm-latex-inline';
+
+    try {
+      katex.render(expr, span, {
+        displayMode,
+        throwOnError: false,
+        output: 'html'
+      });
+    } catch (e) {
+      span.textContent = `$${expr}$`;
+      span.className = 'cm-latex-error';
+    }
+
+    return span;
+  }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+const latexHighlightField = StateField.define<DecorationSet>({
+  create(state: { doc: { toString: () => string; }; }) {
+	return highlightLatex(state.doc.toString());
+  },
+  update(decorations: any, tr: { docChanged: any; state: { doc: { toString: () => string; }; }; }) {
+	return tr.docChanged ? highlightLatex(tr.state.doc.toString()) : decorations;
+  }
+});
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+function highlightLatex(text: string): DecorationSet {
+  const decorations: any[] = [];
+  let match;
+  while ((match = LATEX_REGEX.exec(text)) !== null) {
 
-	display(): void {
-		const {containerEl} = this;
+    const from = match.index;
+    const to = from + match[0].length;
+    decorations.push(
+      Decoration.mark({
+        attributes: { class: 'cm-latex-formula' , spellcheck: 'false'},
+      }).range(from, to)
+    );
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    decorations.push(
+      Decoration.widget({
+        widget: new LatexWidget(match)
+      }).range(to)
+    );
+  }
+  return Decoration.set(decorations, true);
 }
+
+export const latexHighlightExtension = [
+  latexHighlightField,
+  EditorView.decorations.from(latexHighlightField, (d: any) => d)
+];
